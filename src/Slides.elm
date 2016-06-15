cc53209ee176fdff8cab759d59db70d958bd83f9
xarvh <francesco.orsenigo@gmail.com>
2016-06-15 12:12:58 +1000

    Add animation calculation

--- src/Slides.elm
+++ src/Slides.elm
@@ -8,6 +8,7 @@ module Slides exposing
     )
 
 
+import AnimationFrame
 import Array exposing (Array)
 import Html exposing (..)
 import Html.Attributes exposing (class, style)
@@ -17,6 +18,7 @@ import Markdown exposing (defaultOptions)
 import Mouse
 import String
 import Task
+import Time
 import Window
 
 
@@ -33,6 +35,9 @@ slidePixelSize =
     { height = 700
     , width = 960
     }
+
+animationDuration =
+    3000 * Time.millisecond
 
 keyCodesToMessage =
     [   { message = First
@@ -89,20 +94,31 @@ unindent multilineString =
 --
 type Message
     = Noop
+
     | First
     | Last
     | Next
     | Prev
+
+    | AnimationTick Time.Time
+
     | WindowResizes Window.Size
+
+
+type AnimationStatus
+    = Idle
+    | Transitioning Int Float
 
 
 type alias Slide =
     { content : Html Message
     }
 
+
 type alias Model =
     { slides : Array Slide
     , currentSlideIndex : Int
+    , animationStatus : AnimationStatus
     , scale : Float
     }
 
@@ -130,7 +146,7 @@ md markdownContent =
 init : List Slide -> (Model, Cmd Message)
 init slides =
     let
-        model = Model (Array.fromList slides) 0 1.0
+        model = Model (Array.fromList slides) 0 Idle 1.0
         cmd = Task.perform (\_ -> Noop) WindowResizes Window.size
     in
         (model, cmd)
@@ -165,16 +181,40 @@ update message oldModel =
         noCmd m =
             (m, Cmd.none)
 
+
         selectSlide newIndex =
-            noCmd { oldModel | currentSlideIndex = clamp 0 (Array.length oldModel.slides - 1) newIndex }
+            let
+                newIndex = clamp 0 (Array.length oldModel.slides - 1) newIndex
+                newModel = { oldModel | animationStatus = Transitioning newIndex 0 }
+            in
+                noCmd <|
+                    if newIndex == oldModel.currentSlideIndex || oldModel.animationStatus /= Idle
+                    then oldModel
+                    else newModel
+
+
     in
         case message of
             Noop -> noCmd oldModel
+
             First -> selectSlide 0
             Last -> selectSlide 99999
             Prev -> selectSlide <| oldModel.currentSlideIndex - 1
             Next -> selectSlide <| oldModel.currentSlideIndex + 1
+
             WindowResizes size -> noCmd <| windowResize oldModel size
+
+            AnimationTick deltaTime ->
+                case oldModel.animationStatus of
+                    Idle -> noCmd oldModel
+                    Transitioning endSlideIndex completion ->
+                        let
+                            newCompletion = completion + deltaTime / animationDuration
+                        in
+                            noCmd <|
+                                if newCompletion >= 1
+                                then { oldModel | currentSlideIndex = endSlideIndex, animationStatus = Idle }
+                                else { oldModel | animationStatus = Transitioning endSlideIndex newCompletion }
 
 
 
@@ -211,6 +251,11 @@ view model =
                 ]
             ]
             [ (currentSlide model).content ]
+
+        , text <| case model.animationStatus of
+            Idle -> "idle"
+            Transitioning direction completion ->
+                toString direction ++ "  " ++ toString completion
         ]
 
 
@@ -223,6 +268,7 @@ keyPressDispatcher keyCodeMap keyCode =
         x :: xs -> if List.member keyCode x.keyCodes then x.message else keyPressDispatcher xs keyCode
         _ -> Noop
 
+
 mouseClickDispatcher position =
     Next
 
@@ -234,6 +280,7 @@ subscriptions model =
     [ Keyboard.ups (keyPressDispatcher keyCodesToMessage)
     , Mouse.clicks mouseClickDispatcher
     , Window.resizes WindowResizes
+    , AnimationFrame.diffs AnimationTick
     ]
 
 
